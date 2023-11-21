531cfbb87748548b090b8151f14162abb1d62692
ebin6238 <ebinbaby2000@gmail.com>
2023-11-21 16:51:14 +0530

    calculator design

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+const displayData =  (data)=>{
+result.value += data
+}
+
+const calculatorClear =()=>{
+    result.value=""
+}
+
+const calculateResult = ()=>{
+    if(result.value!=""){
+  try{ result.value=  eval(result.value) }
+  catch{
+    result.value="Error!!"
+  }
+}
+  
+}
+
+const removeLast = ()=>{
+    result.value = result.value.slice(0,-1)
+}
